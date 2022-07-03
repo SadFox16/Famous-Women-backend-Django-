9d4e12bbedc8cf20ecd2983d2564ac4cb8c2dd6e
SadFox16 <oleg.s.kerimov@yandex.com>
2022-07-03 14:33:59 +0300

    Add registration and authentication

---
+++ famouswomen/women/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Women(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Ф.И.О')
+    content = models.TextField(blank=True, verbose_name='Краткая биография')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    user = models.ForeignKey(User, verbose_name='Кем добавлена', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Женщина"
+        verbose_name_plural = "Женщины"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True, verbose_name='Наименование')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
